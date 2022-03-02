012d5f02dcc248111195f1e7806a487c07d034ec
José Cleiton C. Santos <69883217+jose-cleiton@users.noreply.github.com>
2022-03-02 12:24:30 -0300

    Projeto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import "./styles.css";
+import React from "react";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Tryunfo</h1>
+      </div>
+    );
+  }
 }
+
+export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Form extends React.Component {
+  render() {
+    return <h1>Ok</h1>;
+  }
+}
+
+export default Form;
